6f889f7376c65d65674217a33ab570f8c360a888
farrukh-nadeem <74743621+farrukh-nadeem@users.noreply.github.com>
2020-11-20 06:54:17 +0500

    Update settings.py

--- gym/settings.py
+++ gym/settings.py
@@ -27,7 +27,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['leanclan.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
